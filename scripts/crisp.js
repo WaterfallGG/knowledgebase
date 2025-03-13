267777a930d061d1f42051f752722d6e5b68b256
ItsAllay <106814226+ItsAllay@users.noreply.github.com>
2025-03-13 11:32:34 +0000

    Documentation edits made through Mintlify web editor

---
+++ scripts/crisp.js
@@ -0,0 +1,13 @@
+if (typeof window !== "undefined") {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "cd0c87b2-b361-44f3-b60b-1c85caa7bfe1";
+  
+    (function () {
+      const d = document;
+      const s = d.createElement("script");
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = true;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  }
+  
